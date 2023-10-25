c8b1bed83c40bce06250b065d652814a6a92cd15
Rémi DEROM <remi.derom@gmail.com>
2023-10-25 13:51:44 +0200

    start project

---
+++ app/src/main/java/com/standardListAndDetailApp/ListAndDetailApplication.kt
@@ -0,0 +1,10 @@
+package com.standardListAndDetailApp
+
+import android.app.Application
+
+class ListAndDetailApplication : Application() {
+
+
+
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+
+rootProject.name = "StandardListAndDetilApplicationContent"
+include(":app")
+include(":app:feature")
+include(":features")
+include(":features:listanddetail")
